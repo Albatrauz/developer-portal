962b3b2f68e9422a1bae25d6118a1197fd8b7337
Albatrauz <michaelvg94@gmail.com>
2023-10-05 22:41:54 +0200

    Init commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core'
+const store = useLocalStorage(
+      'my-storage',
+      {
+        name: 'Apple',
+        color: 'red',
+      },
+    )
+
+    const ui = {
+      background: 'bg-white dark:bg-cyan-950 text-emerald-400',
+    }
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <UCard :ui="ui">
+      <h1>Dit is een test</h1>
+    </UCard>
+
+  </div>
+</template>
